2fd1ff8f8dd0de0ca07ecc436fae76f5af6417fd
Brian Kariu <waiganjo.brian@gmail.com>
2024-04-04 20:59:59 +0300

    feat: Initialized blog repostory
    - Created Astro repository

---
+++ src/data/categories.ts
@@ -0,0 +1,13 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Category 1',
+	'Category 2',
+	'Category 3',
+	'Category 4',
+	'Category 5',
+	'Technology and Software Development',
+	'Book Reviews and Recommendations',
+	'Personal Development for Developers',
+	'Personal Projects and Experiments',
+	'Lifestyle and Interests'
+] as const
